2cd25dda7d6a4cf824d161d9041b2cc7df314107
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2023-05-26 13:41:33 -0400

    Messages Are Now Visible

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,5 @@
-import {useState} from 'react';
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import {useEffect, useState} from 'react';
+import { addDoc, collection, serverTimestamp, onSnapshot, query, where } from "firebase/firestore";
 import { auth, db } from "../config/firebase.js";
 import '../styles/Chat.css';
 
@@ -7,8 +7,20 @@ export const Chat = (props) => {
 
     const {room} = props;
     const [newMessage, setNewMessage] = useState("");
+    const [messages, setMessages] = useState([]);
 
     const messagesRef = collection(db, "messages");
+
+    useEffect(() => {
+        const queryMessages = query(messagesRef, where("room", "==", room));
+        onSnapshot(queryMessages, (snapshot) => {
+            let messages = [];
+            snapshot.forEach((doc) => {
+                messages.push({...doc.data(), id: doc.id})
+            });
+            setMessages(messages);
+        });
+    }, []);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -26,6 +38,8 @@ export const Chat = (props) => {
 
     return (
         <div className="chat-app">
+            <div>
+                {messages.map((message) => <h1>{message.text}</h1>)}</div>
             <form onSubmit={handleSubmit} className="new-message-form">
                 <input 
                     className="new-message-input" 
